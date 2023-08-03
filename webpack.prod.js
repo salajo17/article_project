8ac713fdb7e4606712e837a4615ba71aa2dbb02a
Salayo Din <salajo17@gmail.com>
2023-08-03 01:55:43 -0400

    jest commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require("workbox-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
@@ -8,7 +9,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
@@ -18,6 +19,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
