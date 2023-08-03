317a1b04b794eb235f215affd9af5f730204bb8f
Salayo Din <salajo17@gmail.com>
2023-08-03 01:30:56 -0400

    sec commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+
+var meaningCloud = require("meaningCloud_textapi");
 
 const app = express()
 
@@ -10,7 +14,7 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
@@ -21,3 +25,7 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+var textapi = new meaningCloud({
+    application_key: process.env.API_KEY
+});
